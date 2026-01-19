22ef798e250a3f71570d43023f6b89cca0774959
FionaZZ92 <Fiona.Zhao@amd.com>
2026-01-19 17:18:20 +0800

    Add run_ir mode to test modified ir

--- common/compiler_pass.py
+++ common/compiler_pass.py
@@ -8,8 +8,12 @@ import triton.language as tl
 from triton._C.libtriton import ir, passes
 
 class PassOptIR:
-    def __init__(self, source_file: str):
-        self.source_file = source_file
+    def __init__(self, args):
+        self.source_file = args.source_file
+        self.matrix_instr_nonkdim = args.matrix_instr_nonkdim
+        self.kpack = args.kpack
+        self.num_warps = args.num_warps
+        self.num_stages = args.num_stages
     
     
     def generate_ttir_with_passes(self, output_file) -> None:
@@ -19,7 +23,7 @@ class PassOptIR:
         try:
             target = triton.runtime.driver.active.get_current_target()
             backend = triton.compiler.compiler.make_backend(target)
-            codegen_fns = dict()
+            #codegen_fns = dict()
             module_map = backend.get_module_map()
             context = ir.context()
             ir.load_dialects(context)
@@ -33,13 +37,30 @@ class PassOptIR:
             pm.enable_debug()
             
             print("===Applying Pass...")
+            #print(f"passes.common: {dir(passes.common)}")
+            #print(f"passes.ttir: {dir(passes.ttir)}")
             passes.common.add_inliner(pm)
+            #重写张量指针（tensor pointer）操作‌，以优化内存访问模式并提高性能
             passes.ttir.add_rewrite_tensor_pointer(pm)
+            #将涉及张量描述符（tensor descriptor）的操作重写为使用张量指针（tensor pointer）的特定模式‌
+            passes.ttir.add_rewrite_tensor_descriptor_to_pointer(pm)
+            #标准化和规范化，如x*1->x
             passes.common.add_canonicalizer(pm)
+            #tt.dot 操作和 arith.addf 操作融合
             passes.ttir.add_combine(pm)
+            #broadcast和splat操作移动到elementwise操作的后面
             passes.ttir.add_reorder_broadcast(pm)
+            #公共子表达式消除
             passes.common.add_cse(pm)
+            #静态单赋值形式下的公共子表达式消除
+            passes.common.add_sccp(pm)
+            #循环感知的公共子表达式消除
+            passes.ttir.add_loop_aware_cse(pm)
+            #循环不变量外提
+            passes.ttir.add_triton_licm(pm)
+            #死代码消除‌
             passes.common.add_symbol_dce(pm)
+            #循环展开
             passes.ttir.add_loop_unroll(pm)
             print("===Executing Pass optimization...")
             pm.run(module)
@@ -47,19 +68,105 @@ class PassOptIR:
             with open(output_file, 'w') as f:
                 f.write(str(module))
             print(f"[DONE] TTIR saved in: {output_file}")
+
         except Exception as e:
             print(f"[ERROR] Generate/pass TTIR failed: {e}")
 
-    def generate_ttgir_with_passes(self, output_file) -> None:
-        return
+
+
+    def generate_ttgir_with_passes(self, output_file, config) -> None:
+        if self.source_file == None:
+            print("[ERROR] empty ttir code")
+            return
+        try:
+            target = triton.runtime.driver.active.get_current_target()
+            backend = triton.compiler.compiler.make_backend(target)
+            module_map = backend.get_module_map()
+            context = ir.context()
+            ir.load_dialects(context)
+            backend.load_dialects(context)
+            module = ir.parse_mlir_module(self.source_file, context)
+
+            # create pass manager
+            pm = ir.pass_manager(context)
+            pm.enable_debug()
+            
+            passes.ttir.add_convert_to_ttgpuir(pm, f"hip:{target.arch}", self.num_warps, target.warp_size, 1) #1 CTA for kernel execution 
+            pm.run(module, 'make_ttgir_stage1')
+
+            pm = ir.pass_manager(context)
+            pm.enable_debug()
+
+            print("===Applying Pass...")
+
+            passes.ttgpuir.add_coalesce(pm)
+            passes.ttgpuir.add_f32_dot_tc(pm, False)
+            passes.ttgpuir.add_remove_layout_conversions(pm)
+            passes.ttgpuir.add_optimize_thread_locality(pm)
+            #For GEMM kernels on an AMD MI300X accelerator, mfma_16x16 typically outperforms mfma_32x32, even for large tile/GEMM sizes.
+            amd.passes.ttgpuir.add_accelerate_matmul(pm, target.arch, self.matrix_instr_nonkdim, self.kpack)
+            passes.ttgpuir.add_remove_layout_conversions(pm)
+            amd.passes.ttgpuir.add_optimize_epilogue(pm)
+            amd.passes.ttgpuir.add_optimize_dot_operands(pm, target.arch)
+            amd.passes.ttgpuir.add_hoist_layout_conversions(pm)
+            amd.passes.ttgpuir.add_sink_layout_conversions(pm)
+
+            passes.ttgpuir.add_fuse_nested_loops(pm)
+            passes.common.add_canonicalizer(pm)
+            passes.ttir.add_triton_licm(pm)
+            passes.common.add_canonicalizer(pm)
+
+            amd.passes.ttgpuir.add_schedule_loops(pm, 2) #num_stages is 2, 3 for Nv
+            passes.common.add_canonicalizer(pm)
+            #try scheduler hint later
+            # for hint in str(""attention,memory-bound-attention").split(","):
+            #    amd.passes.ttgpuir.insert_instruction_sched_hints(pm, hint)
+            passes.ttgpuir.add_remove_layout_conversions(pm)
+            passes.ttgpuir.add_reduce_data_duplication(pm)
+
+            if target.arch == "gfx942":
+                amd.passes.ttgpuir.add_pipeline(pm, False, True) 
+                #特定的张量转置操作（transpose）优化为线程级（in-thread）操作
+                amd.passes.ttgpuir.add_in_thread_transpose(pm)
+                #移除不必要的layout转换操作
+                passes.ttgpuir.add_remove_layout_conversions(pm)
+            elif target.arch == "gfx950":
+                amd.passes.ttgpuir.add_pipeline(pm, True, True) #use_async_copy & use_block_pingpong
+                amd.passes.ttgpuir.add_coalesce_async_copy(pm, target.arch)
+            else:
+                amd.passes.ttgpuir.add_pipeline(pm, False, False) 
+            amd.passes.ttgpuir.add_reorder_instructions(pm)
+            amd.passes.ttgpuir.add_block_pingpong(pm, self.num_stages)
+
+            amd.passes.ttgpuir.add_fold_true_cmpi(pm)
+            passes.common.add_canonicalizer(pm)
+            passes.common.add_cse(pm)
+            passes.common.add_symbol_dce(pm)
+            print("===Executing Pass optimization...")
+            pm.run(module)
+            print("[DONE]Generate TTGIR successfully.")
+            with open(output_file, 'w') as f:
+                f.write(str(module))
+            print(f"[DONE] TTGIR saved in: {output_file}")
+
+        except Exception as e:
+            print(f"[ERROR] Generate/pass TTGIR failed: {e}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,description="select test",)
     parser.add_argument("--source_file", "-s", type=str, default="")
+    parser.add_argument("--ttgir", "-ttg", action="store_true")
+    parser.add_argument("--num_warps", "-nw", type=int, default=4)
+    parser.add_argument("--num_stages", "-ns", type=int, default=2)
+    parser.add_argument("--matrix_instr_nonkdim", "-min", type=int, default=16)
+    parser.add_argument("--kpack", type=int, default=1)
     args = parser.parse_args()
     if not os.path.exists(args.source_file):
         print(f"[ERROR] {args.source_file} not exists.")
         sys.exit(1)
-    obj = PassOptIR(args.source_file)
-    obj.generate_ttir_with_passes("new.ttir")
+    obj = PassOptIR(args)
+    if args.ttgir:
+        obj.generate_ttgir_with_passes("new.ttgir")
+    else:
+        obj.generate_ttir_with_passes("new.ttir")
--- utest/test_moe.py
+++ utest/test_moe.py
@@ -19,7 +19,9 @@ from vllm.model_executor.layers.fused_moe.config import (
 import inspect
 import ast
 import shutil
-from pathlib import Path
+import pathlib
+import re
+import fnmatch
 
 #<=256
 aiter_small_moe_config = {
@@ -137,7 +139,7 @@ def check_best_config(method):
     config_file_path = f"moe_{method}_best_config.json"
     if os.path.exists(config_file_path):
         with open(config_file_path) as f:
-            print(f"=====Using configuration from {config_file_path} for MoE layer.")
+            print(f"[INFO] Using configuration from {config_file_path} for MoE layer.")
             # If a configuration has been found, return it
             best_config = json.load(f)
             return best_config
@@ -152,25 +154,27 @@ def set_triton_env_vars(output_dir):
     os.environ['TRITON_COMPILER_DUMP_PATH'] = str(output_dir)
     os.environ['TRITON_ALWAYS_COMPILE'] = '1'
     os.environ['TRITON_DEBUG'] = '1'
+    os.environ['TRITON_CACHE_DIR'] = "./"
 
 def save_to_file(filename, content):
     try:
         abs_path = os.path.abspath(filename)
         with open(filename, "w", encoding="utf-8") as f:
             f.write(content)
-        print(f"[save ast succeed] -> save into file path: {abs_path}")
+        print(f"[INFO] save ast succeed -> save into file path: {abs_path}")
     except Exception as e:
-        print(f"[save ast failed] -> error {e}")
+        print(f"[ERROR] save ast failed -> error {e}")
 
 def save_cache_to_export(method):
-    cache_dir = Path.home() / ".triton" / "cache"
+    cache_dir = os.getenv('TRITON_CACHE_DIR')
+    if cache_dir is None:
+        cache_dir = pathlib.Path.home() / ".triton" / "cache"
     export_dir = f"{method}_cache/"
-    #os.makedirs(export_dir, exist_ok=True)
     if cache_dir.exists():
         shutil.copytree(cache_dir, export_dir, dirs_exist_ok=True)
     else:
-        print("=====Not remain ~/.triton/cache!!!=====")
-    shutil.rmtree(cache_dir)
+        print("[ERROR] Not remain ~/.triton/cache!!!")
+    #shutil.rmtree(cache_dir)
 
 def save_ast(method):
     if "aiter" in method:
@@ -185,7 +189,67 @@ def save_ast(method):
         ast_dump_str = ast.dump(kernel_ast)
     ast_filename = f"moe_{method}.ast.txt"
     save_to_file(ast_filename, ast_dump_str)
+
+def replace_files_with_pattern(root_path, file_pattern, new_pattern):
     
+    # 使用lambda表达式和fnmatch进行模式匹配
+    pattern_match = lambda file_name, pat: fnmatch.fnmatch(file_name, pat)
+    
+    #matched_files = []
+    
+    for file_path in root_path.rglob('*'):
+        if file_path.is_file() and pattern_match(file_path.name, file_pattern):
+            suffix = file_path.suffix
+            escaped_pattern = re.escape(file_pattern)
+            regex_pattern = escaped_pattern.replace(r'\*', '.*') 
+            #'(^__grp__.*@e2e_moe_persistent_kernel_top_k_.*?|.*@e2e_moe_persistent_kernel_top_k_.*?)\\.(.+)$'
+            pattern_grp = f'(^__grp__{regex_pattern}?|{regex_pattern}?)\.(.+)$'
+            match = re.match(pattern_grp, str(file_path.name))
+            if match:
+                if match.group(1).startswith('__grp__'):
+                    new_name = "__grp__" + new_pattern + suffix
+                else:
+                    new_name = new_pattern + suffix
+                new_path = file_path.parent / new_name
+                try:
+                    file_path.rename(new_path)
+                    print(f"[INFO] Rename: {file_path.name} -> {new_name}")
+                except Exception as e:
+                    print(f"[INFO] Rename failed {file_path.name}: {e}")
+            else:
+                print(f"[ERROR] replace cache failed.")
+        
+    
+def compile_ir(args):
+    try: 
+        if args.method == "aiter_persistent":
+            file_pattern = "*@e2e_moe_persistent_kernel_top_k_*"
+            new_pattern = "e2e_moe_persistent_kernel"
+            kwargs = aiter_p_small_moe_config
+        elif args.method == "aiter":
+            file_pattern = "*@e2e_moe_kernel_top_k_*"
+            new_pattern = "e2e_moe_kernel"
+            kwargs = aiter_small_moe_config
+        else:
+            file_pattern = "*@fused_moe_kernel*"
+            new_pattern = "fused_moe_kernel"
+            kwargs = vllm_small_moe_config
+        tuned_config = check_best_config(args.method)
+        if tuned_config is not None and str(args.input_token) in tuned_config:
+            kwargs = tuned_config[str(args.input_token)]
+        target = triton.runtime.driver.active.get_current_target()
+        backend = triton.compiler.compiler.make_backend(target)
+        options = backend.parse_options(kwargs)
+        kernel = triton.compile(str(args.ir_file), target=target, options=options.__dict__)
+        print(f"[INFO] kernel.metadata:{kernel.metadata}")
+    except Exception as e:
+        print(f"[ERROR] compile/run IR failed: {e}")
+
+    
+    target_directory = os.getenv('TRITON_CACHE_DIR')
+    if target_directory is None:
+        target_directory = pathlib.Path.home() / ".triton" / "cache"
+    replace_files_with_pattern(target_directory, file_pattern, new_pattern)
 
 
 
@@ -234,7 +298,7 @@ def runner_test(input_token, method, inter_dim, hidden_size, experts, topk, dtyp
                 tuned_config = check_best_config(method)
                 if tuned_config is not None and str(input_token) in tuned_config:
                     config = tuned_config[str(input_token)]
-                    print(f"Use config of input_tokn {input_token}")
+                    print(f"[INFO] Use config of input_token {input_token}")
             if config == None: #non autotune mode
                 config = aiter_p_small_moe_config
             config_m_block = config['BLOCK_SIZE_M']
@@ -310,12 +374,12 @@ def runner_test(input_token, method, inter_dim, hidden_size, experts, topk, dtyp
                 rep=10,
             )
         else:
-            #if usebest:
-            #    config = check_best_config(method)
+            if usebest:
+                config = check_best_config(method)
             if config == None:
                 if input_token == 1:
                     config = vllm_small_moe_config
-                    print(f"=====Use vllm_small_moe_config:{vllm_small_moe_config}")
+                    print(f"[INFO] Use vllm_small_moe_config:{vllm_small_moe_config}")
             ms, min_ms, max_ms = testing.do_bench(lambda: fused_experts(
                     x,
                     w1,
@@ -332,13 +396,13 @@ def runner_test(input_token, method, inter_dim, hidden_size, experts, topk, dtyp
                 rep=10,
             )
     except ValueError as ve:
-        print(f"config value error: {ve}")
+        print(f"[ERROR] config value error: {ve}")
         return None
     except triton.TritonError as e:
-        print(f"triton kernel error: {e}")
+        print(f"[ERROR] triton kernel error: {e}")
         return None
     except Exception as e:
-        print(f"Unexpect error: {e}")
+        print(f"[ERROR] Unexpect error: {e}")
         traceback.print_exc()
         return None
     #end = time.time()
@@ -360,11 +424,12 @@ def main(args:argparse.ArgumentParser):
         'bfloat16': torch.bfloat16,
         'float8_e4m3fn': torch.float8_e4m3fn,
     }
+
     if args.mode == "autotune":
         if args.usebest:
-            print("=====ignore usebest for autotune=====")
+            print("[WARNING] ignore usebest for autotune.")
         if args.cache:
-            print("=====ignore cache for autotune=====")
+            print("[WARNING] ignore cache for autotune.")
         tune_config = hip_autotune_config(dtype_map[args.dtype], args.method)
         config_time_data = []
         for i in tune_config:
@@ -385,14 +450,14 @@ def main(args:argparse.ArgumentParser):
                 }
                 config_time_data.append(tmp)
         config_time_data = sorted(config_time_data, key=lambda x: x.get("latency"), reverse=False)
-        #print(f"Available config:{config_time_data}")
+        token_config = {
+            f"{args.input_token}": config_time_data[0].get("name")
+        }
         with open(f"moe_{args.method}_best_config.json", 'w') as f:
-            json.dump(config_time_data[0].get("name"), f, indent=2)
-        
-
+            json.dump(token_config, f, indent=2)
     elif args.mode == "benchmark":
         if args.cache:
-            print("=====ignore cache for autotune=====")
+            print("[WARNING] ignore cache for autotune")
         config = None
         bench_config = [testing.Benchmark(
             x_names=['input_token'],
@@ -409,7 +474,7 @@ def main(args:argparse.ArgumentParser):
         )]
         bench_func=benchmark_runner(bench_config, runner_test)
         bench_func.run(show_plots=True, print_data=True)
-    else:
+    elif args.mode == "run":
         if args.cache:
             save_ast(args.method)
         config = None
@@ -430,6 +495,32 @@ def main(args:argparse.ArgumentParser):
         run_func.run(show_plots=True, print_data=True)
         if args.cache:
             save_cache_to_export(args.method)
+    else: #run_ir
+        if os.path.exists(args.ir_file) and os.path.isfile(args.ir_file):
+            compile_ir(args)
+            print("[INFO]Load and compile works.")
+            config = None
+            run_config = [testing.Benchmark(
+                x_names=['input_token'],
+                x_vals= [args.input_token],
+                line_arg='method',
+                line_vals=[args.method],
+                line_names=[args.method],
+                styles=[('red', '-')],
+                ylabel='ms',
+                plot_name='moe-test',
+                args={"inter_dim": args.inter_dim, "hidden_size": args.hidden_size, 
+                    "experts": args.experts, "topk": args.topk, 
+                    "dtype": dtype_map[args.dtype], "config": config, "usebest": args.usebest}
+            )]
+            run_func = benchmark_runner(run_config, runner_test)
+            run_func.run(show_plots=True, print_data=True)
+            if args.cache:
+                save_cache_to_export(args.method)
+        else:
+            print("[ERROR] Cannot find ir_file.")
+
+            
 
 
 
@@ -448,9 +539,10 @@ if __name__ == "__main__":
     )
     #group = parser.add_mutually_exclusive_group(required=True)
     parser.add_argument(
-        "--mode", type=str, choices=["run", "benchmark", "autotune"], default="run"
+        "--mode", type=str, choices=["run", "run_ir", "benchmark", "autotune"], default="run"
     )
     parser.add_argument("--usebest", action="store_true", default=True)
     parser.add_argument("--cache", action="store_true")
+    parser.add_argument("--ir_file", type=str, default="")
     args = parser.parse_args()
     main(args)
